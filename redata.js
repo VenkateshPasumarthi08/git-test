38fed6a8248b2fc4b2e2252f723f287cc7e5b8a6
Venkatesh <nagavenkat783@gmail.com>
2024-04-22 15:21:03 +0530

    lines deleted

--- redata.js
+++ redata.js
@@ -3,11 +3,7 @@ import React from 'react'
 function  redata() {
   return (
     <div>
-        this is using for checking branch
-        this is using for checking branch 
-        this is using for checking branch
-        this is using for checking branch
-        this is using for checking branch </div>
+        </div>
   )
 }
 
