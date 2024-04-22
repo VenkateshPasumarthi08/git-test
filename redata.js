f56ac71fc756e3f8177df0f6493ad3dd670bdc2e
Venkatesh <nagavenkat783@gmail.com>
2024-04-22 15:36:55 +0530

    two lines added

--- redata.js
+++ redata.js
@@ -3,7 +3,9 @@ import React from 'react'
 function  redata() {
   return (
     <div>
-        here are we are learning about branches
+        here we are learning about branches
+        here we are learning about branches 
+        here we are learning about branches
     </div>
   )
 }
